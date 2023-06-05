fd402657a79e80fac576a5576980ba696f4962f6
BoKyeong Kim <93707167+ym1021@users.noreply.github.com>
2023-06-05 23:48:10 +0900

    Delete routes directory

--- routes/menu.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import menuController from '../controller/menuController.js';
-
-const router = express.Router();
-
-router.get('/name', menuController.name);
-router.get('/select', menuController.select);
-
-export default router;
--- routes/restaurant.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import resController from '../controller/resController.js';
-
-const router = Router();
-
-router.get('/select', resController.select);
-router.get('/name', resController.name);
-
-export default router;
